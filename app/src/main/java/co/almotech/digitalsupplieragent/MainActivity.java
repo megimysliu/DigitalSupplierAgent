209fae818ff2648b0a3330e3f2055ca474315cf5
megi <megi.mysliu@gmail.com>
2020-10-05 11:33:24 +0200

    New layout and icons

--- app/src/main/java/co/almotech/digitalsupplieragent/MainActivity.java
+++ app/src/main/java/co/almotech/digitalsupplieragent/MainActivity.java
@@ -2,13 +2,42 @@ package co.almotech.digitalsupplieragent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import co.almotech.digitalsupplieragent.activities.LoginActivity;
+import co.almotech.digitalsupplieragent.activities.RegistrationActivity;
+import co.almotech.digitalsupplieragent.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = mBinding.getRoot();
+        setContentView(view);
+
+        mBinding.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent  = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        mBinding.register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent  = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
