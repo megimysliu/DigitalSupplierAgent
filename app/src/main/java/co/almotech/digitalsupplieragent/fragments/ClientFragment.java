8c377fde296bea429e294a6060cde6ba332c685a
Megi Mysliu <megi.mysliu@gmail.com>
2020-10-06 17:06:23 +0200

    New layout & icons

---
+++ app/src/main/java/co/almotech/digitalsupplieragent/fragments/ClientFragment.java
@@ -0,0 +1,39 @@
+package co.almotech.digitalsupplieragent.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.almotech.digitalsupplieragent.databinding.FragmentClientBinding;
+
+
+public class ClientFragment extends Fragment {
+
+    private FragmentClientBinding mBinding;
+
+    public ClientFragment() {
+
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        mBinding = FragmentClientBinding.inflate(inflater,container,false);
+        View v  = mBinding.getRoot();
+        return v;
+
+    }
+}
---
+++ app/src/main/java/co/almotech/digitalsupplieragent/fragments/InvoiceFragment.java
@@ -0,0 +1,39 @@
+package co.almotech.digitalsupplieragent.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.almotech.digitalsupplieragent.R;
+import co.almotech.digitalsupplieragent.databinding.FragmentInvoiceBinding;
+
+
+public class InvoiceFragment extends Fragment {
+    FragmentInvoiceBinding mBinding;
+
+    public InvoiceFragment() {
+
+    }
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        mBinding = FragmentInvoiceBinding.inflate(inflater,container,false);
+        View v = mBinding.getRoot();
+        return v;
+    }
+}
