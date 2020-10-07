3c8066cabd8039d4bef7e1d00d4e9514d4ba406f
Megi Mysliu <megi.mysliu@gmail.com>
2020-10-07 16:30:17 +0200

    New layout & icons

---
+++ app/src/main/java/co/almotech/digitalsupplieragent/fragments/AgentFragment.java
@@ -0,0 +1,39 @@
+package co.almotech.digitalsupplieragent.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.almotech.digitalsupplieragent.R;
+import co.almotech.digitalsupplieragent.databinding.FragmentAgentBinding;
+
+
+public class AgentFragment extends Fragment {
+
+    private FragmentAgentBinding mBinding;
+
+    public AgentFragment() {
+
+    }
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mBinding = FragmentAgentBinding.inflate(inflater,container,false);
+        View view  = mBinding.getRoot();
+        return view;
+    }
+}
