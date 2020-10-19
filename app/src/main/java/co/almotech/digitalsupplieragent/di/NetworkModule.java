270da9b668986b9ad86191265a8d87614b163313
Megi Mysliu <megi.mysliu@gmail.com>
2020-10-19 17:23:23 +0200

    dependeny injection

--- app/src/main/java/co/almotech/digitalsupplieragent/di/NetworkModule.java
+++ app/src/main/java/co/almotech/digitalsupplieragent/di/NetworkModule.java
@@ -2,11 +2,17 @@ package co.almotech.digitalsupplieragent.di;
 
 import com.google.gson.Gson;
 
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Singleton;
+
 import co.almotech.digitalsupplieragent.network.API;
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.android.components.ApplicationComponent;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -16,17 +22,37 @@ public class NetworkModule {
 
     private static final String BASE_URL = "baseurl";
 
+
+
+    @Singleton
     @Provides
-    public Retrofit provideRetrofit(){
+    public OkHttpClient provideHttpClient(){
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder okHttpClient =   new OkHttpClient().newBuilder();
+        okHttpClient.callTimeout(40, TimeUnit.SECONDS);
+        okHttpClient.connectTimeout(40, TimeUnit.SECONDS);
+        okHttpClient.readTimeout(40, TimeUnit.SECONDS);
+        okHttpClient.writeTimeout(40, TimeUnit.SECONDS);
+        okHttpClient.addInterceptor(loggingInterceptor);
+        return okHttpClient.build();
+    }
+
+    @Singleton
+    @Provides
+    public Retrofit provideRetrofit(OkHttpClient okHttpClient){
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create(new Gson()))
                 .build();
 
         return retrofit;
 
     }
+    @Singleton
     @Provides
     public API provideApi(Retrofit retrofit){
 
